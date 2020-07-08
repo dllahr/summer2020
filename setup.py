b6583bd3996ac22f23c763a67948beb3e9af2fd0
Dave Lahr <dlahr@foghorntx.com>
2020-07-08 07:38:20 -0400

    summer2020py:  rename python to summer2020py, add setup script

---
+++ setup.py
@@ -0,0 +1,5 @@
+import setuptools
+
+setuptools.setup(name="summer2020py", version="0.0.1", description="python code for Summer 2020",
+    packages=setuptools.find_packages(),
+    install_requires=["pandas>=0.20.3", "openpyxl>=2.5.1"])

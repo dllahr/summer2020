9d911e4afde64f78d65d59b8649b060dcfc6853c
avsomers25 <67980093+avsomers25@users.noreply.github.com>
2020-07-13 10:18:16 -0400

    Create AndrewScript.py
    Added a file that is the template with the  snippet file in main, going to be where I make the snippet run from the command line

---
+++ summer2020py/genebody_coverage_prep/AndrewScript.py
@@ -0,0 +1,68 @@
+import logging
+import summer2020py
+import summer2020py.setup_logger as setup_logger
+import argparse
+import sys
+
+
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
+
+def build_parser():
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--verbose", "-v", help="Whether to print a bunch of output.", action="store_true", default=False)
+    parser.add_argument("--hostname", help="lims db host name", type=str, default="getafix-v")
+
+    parser.add_argument("--config_filepath", help="path to config file containing information about how to connect to CDD API, ArxLab API etc.",
+        type=str, default=summer2020py.default_config_filepath)
+    parser.add_argument("--config_section", help="section of config file to use for information about how to connect to CDD API, ArxLab API etc.",
+        type=str, default=summer2020py.default_config_section)
+
+    parser.add_argument("--queue_choice", "-qc", help="which of the queues to work on - valid values are roast, brew, both", type=str,
+        choices=["roast", "brew", "both"], default="both")
+    parser.add_argument("--add_to_queue", "-a", help="add the det_plate entries to the roast_queue", type=str, nargs="+", default=None)
+    # To make --option1 and --option2 mutually exclusive, one can define mutually_exclusive_group in argparse,
+    # argparse asserts that the options added to the group are not used at the same time and throws exception if otherwise
+    mutually_exclusive_group = parser.add_mutually_exclusive_group()
+    mutually_exclusive_group.add_argument("--option1", action="store", dest="option1", help="provide argument for option1", default=None)
+    mutually_exclusive_group.add_argument("--option2", action="store", dest="option2", help="provide argument for option2", default=None)
+    return parser
+
+
+def main(args):
+      if os.path.exists(output.gbdy_prep):
+	print("output directory exists, deleting gbdy_prep:", output.gbdy_prep)
+	shutil.rmtree(output.gbdy_prep)
+
+    print("making output directory gbdy_prep:", output.gbdy_prep)
+    os.mkdir(output.gbdy_prep)
+
+    for cur_sample in SAMPLES:
+	print("cur_sample:", cur_sample)
+	cur_dir = os.path.join(output.gbdy_prep, cur_sample)
+	os.mkdir(cur_dir)
+
+	cur_search = os.path.join(input.dir, cur_sample + "*")
+	print("cur_search:", cur_search)
+	cur_file_list = glob.glob(cur_search)
+	cur_file_list.sort()
+	print("cur_file_list:", cur_file_list)
+
+	for cur_file in cur_file_list:
+	    basename = os.path.basename(cur_file)
+	    dst = os.path.join(cur_dir, basename)
+	    print("dst:", dst)
+
+	    os.symlink(cur_file, dst)
+
+
+
+
+if __name__ == "__main__":
+    args = build_parser().parse_args(sys.argv[1:])
+
+    setup_logger.setup(verbose=args.verbose)
+
+    logger.debug("args:  {}".format(args))
+
+    main(args)

702ad0f67330d161e377ef9051f127430d6f84aa
avsomers25 <67980093+avsomers25@users.noreply.github.com>
2020-07-15 15:23:26 -0400

    updating scripts
    the unittest are being written the files that are still here will be gone eventually I am working on getting the test working and while I'm doing that I don't delete the files after it runs so I can check to see if it is there myself to debug the test

--- summer2020py/genebody_coverage_prep/genebody_coverage_prep.py
+++ summer2020py/genebody_coverage_prep/genebody_coverage_prep.py
@@ -24,10 +24,9 @@ def build_parser():
         type=str, default=summer2020py.default_config_filepath)
     parser.add_argument("--config_section", help="section of config file to use for information about how to connect to CDD API, ArxLab API etc.",
         type=str, default=summer2020py.default_config_section)
-
-    
-
     return parser
+
+
 
 def main(args):
     Sample_list = None
@@ -44,8 +43,12 @@ def main(args):
     for sample in Sample_list: #each sample in the sample list runs through the for loop
         prepare_sample_dir(sample,args.out_dir,args.input_dir) #calls the method prepare-sample-dir on the sample that is currently iterating
 
+
+
 def load_sample(Sample_File):
     return [] #taking in a .grp file returns a list of all the sample in that file
+
+
 
 def prepare_output_dir(output_dir_path):
     if os.path.exists(output_dir_path): #if the path output_dir_path exists 
@@ -55,17 +58,22 @@ def prepare_output_dir(output_dir_path):
     logger.debug("making output directory output_dir_path: {}".format(output_dir_path))
     os.mkdir(output_dir_path) #create the directory output_dir_path
 
+
+
 def prepare_sample_dir(sample, out_dir,input_dir):
     cur_dir = make_sample_dir(sample, out_dir)
     Sample_input_files = find_sample_input_files(sample, input_dir)
     for input_file in Sample_input_files:
         create_sample_symlink(input_file,cur_dir)
 
+
+
 def make_sample_dir(sample, out_dir):
     logger.debug(" sample: {}".format(sample))
-    cur_dir = os.path.join(sample, out_dir) #create a variable to store where you want the directory to go 
+    cur_dir = os.path.join(out_dir, sample) #create a variable to store where you want the directory to go 
     os.mkdir(cur_dir)
     return cur_dir #return where the directory is so that it can be saved to a variable to use elsewhere
+
 
 
 def find_sample_input_files(sample, input_dir):
@@ -77,11 +85,27 @@ def find_sample_input_files(sample, input_dir):
     return cur_file_list
 
 
+
 def create_sample_symlink(input_file, cur_dir):
-    basename = os.path.basename(input_file) #Return the base name of pathname path and set it equal to basename
+    basename = os.path.basename(input_file) #Return the base name of input_file and set it equal to basename
     dst = os.path.join(cur_dir, basename)#join the paths cur_dir and basename and set it equal to dst
     logger.debug("dst: {}".format(dst))
     os.symlink(input_file, dst)#creates symbolic link to input_file dst
+
+
+
+def create_samples_for_testing(ending, dir):
+    name = "sample_{}".format(ending)
+    sample = os.path.join(dir, name)
+    f = open(sample,"w")#opening the file in write mode
+    f.write("1") #adding the number 1 to the file
+    f.close()#closing the file
+    not_sample = os.path.join(dir,ending)
+    f = open(not_sample,"w")#opening the file in write mode
+    f.write("1") #adding the number 1 to the file
+    f.close()#closing the file
+
+
 
 def snippet(SAMPLES,output,inputs):
     # if os.path.exists(output.gbdy_prep):
--- summer2020py/genebody_coverage_prep/test/test_genebody_coverage_prep.py
+++ summer2020py/genebody_coverage_prep/test/test_genebody_coverage_prep.py
@@ -29,9 +29,20 @@ class TestGenebodyCoveragePrep(unittest.TestCase):
     def test_main(self):
         #gcp.main(None)
         pass
+   
+   
+    def test_create_samples_for_testing(self):
+        testend = "july_first"
+        test_dir = "creationtesting"
+        os.mkdir(test_dir)
+        gcp.create_samples_for_testing(testend, test_dir)
+        #confirming that the files in the directory are sample_june_first and june_first
+        self.assertTrue(os.path.isfile(os.path.join(test_dir, testend)))
+        self.assertTrue(os.path.isfile(os.path.join(test_dir, "sample_" + testend)))
+        shutil.rmtree(test_dir)
+    
     def test_prepare_output_dir(self):
         test_directory = "testing"
-        shutil.rmtree(test_directory)
         self.assertFalse(os.path.exists(test_directory))
         gcp.prepare_output_dir(test_directory)
         self.assertTrue(os.path.exists(test_directory))  #making sure that the output directory was created
@@ -43,6 +54,8 @@ class TestGenebodyCoveragePrep(unittest.TestCase):
         self.assertTrue(os.path.exists(test_directory)) #making sure that the output directory was created
         self.assertFalse(os.path.exists(test_file))#making sure that this is a new directory and not the old one that had a file in it
         shutil.rmtree(test_directory)#delete the test directory
+   
+   
     def test_make_sample_dir(self):
         test_directory = "testing"
         test_sample = "sample"
@@ -51,11 +64,31 @@ class TestGenebodyCoveragePrep(unittest.TestCase):
         self.assertTrue(os.path.exists(new_dir)) #see that the new directory has been created
         shutil.rmtree(new_dir) #delete the new directory 
         shutil.rmtree(test_directory)#delete the test directory
+    
+    
     def test_find_sample_input_files(self):
-        pass
-    def test_create_sample_symlink(self):
-        pass
+        input_directory = "input_test"
+        test_sample ="sample"
+        shutil.rmtree(input_directory)
+        os.mkdir(input_directory)
+        file_list = gcp.find_sample_input_files(test_sample, input_directory)
+        gcp.create_samples_for_testing("one",input_directory)
+        gcp.create_samples_for_testing("two",input_directory)
+        gcp.create_samples_for_testing("three",input_directory)
+        self.assertTrue(file_list)
 
+        
+    def test_create_sample_symlink(self):
+        # test_directory = "testing"
+        # test_sample = "sample"
+        # input_directory = "input_test"
+        # gcp.prepare_output_dir(test_directory)
+        # new_dir = gcp.make_sample_dir(test_sample, test_directory)
+        # file_list = gcp.find_sample_input_files(test_sample, input_directory)
+        # for input_file in file_list:
+        #     gcp.create_sample_symlink(input_file, new_dir)
+        #     #something to confrim it works
+        pass
 
 
 

4971535b0e86b9a90608c31efc84f37399b56784
avsomers25 <67980093+avsomers25@users.noreply.github.com>
2020-07-27 13:07:52 -0400

    Update test_prep_heatmaps.py

--- summer2020py/prep_heatmaps/test/test_prep_heatmaps.py
+++ summer2020py/prep_heatmaps/test/test_prep_heatmaps.py
@@ -10,7 +10,6 @@ import pandas
 import cmapPy.pandasGEXpress.GCToo as GCToo
 import cmapPy.pandasGEXpress.write_gct as write_gct
 import random
-from random import randint
 
 logger = logging.getLogger(setup_logger.LOGGER_NAME)
 
@@ -28,31 +27,26 @@ logger = logging.getLogger(setup_logger.LOGGER_NAME)
 #        self.assertAlmostEquals(...) for comparing floats
 
 temp_wkdir_prefix = "test_prep_heatmaps"
-def randoms():
-    return random.uniform(-10.0, 10.0)
 
-def create_file_for_testing(name, dirs):
-    
-    files = os.path.join(dirs, name)
-
-    f = open(files,"w")#opening the file in write mode
-    f.write("numbers\tlogFC\tt\n1\t{}\t{}\n2\t{}\t{}".format(randoms(),randoms(),randoms(),randoms())) #adding some stuff to the file
-    f.close()#closing the file
-    return files
    
 def random_of_input_file(name, dirs):
-    logger.debug("is this working")
+    #do not believe this works as it should 
     path = os.path.join("assets", "prep_heatmap_example_input_DGE_r10x10.txt")
+    outputpath = os.path.join(dirs, name)
     with open(path) as f_input:
-        first_line = f_input.readline()
-        with open(name, 'w') as f_output:
-                f_output.write(first_line)
-        for line in f_input:
-            data = line.split("\t")
-            for i in range(2,10):
-                data[i] = str(randint(10, 99))
-            with open(name, 'a') as f_output:
-                f_output.write(' '.join(data))
+        with open(outputpath, 'a') as f_output:
+            for line in f_input:
+                f_output.write(line)
+        
+        # first_line = f_input.readline()
+        # with open(outputpath, 'w') as f_output:
+        #         f_output.write(first_line)
+        # for line in f_input:
+        #     data = line.split("\t")
+        #     for i in range(2,len(data)-1):
+        #         data[i] = str(round((random.uniform(-10.0, 10.0)), 5)) + "\t"
+        #     with open(outputpath, 'a') as f_output:
+        #         f_output.write(' '.join(data))
         return name
                 
 
@@ -90,7 +84,7 @@ class TestGeneralPythonScriptTemplate(unittest.TestCase):
             
             #saving the test versions of source_dir and test_id to variables and also creating a test id that won't be tested
             source_dir = os.path.join(wkdir,"source_test")
-            test_id ="124rqtga"
+            test_id ="H202SC20040591"
             notthistest_id = "rnauasf"
             dge_data_test = os.path.join(source_dir, "dge_data")
 
@@ -99,9 +93,9 @@ class TestGeneralPythonScriptTemplate(unittest.TestCase):
             os.mkdir(dge_data_test)
 
             #creating files for the method to find and put into a list
-            create_file_for_testing((test_id + "_one_DGE_rtwo.txt"),dge_data_test)
-            create_file_for_testing((test_id + "_three_DGE_rtwo.txt"),dge_data_test)
-            create_file_for_testing((notthistest_id + "_one_DGE_rtwo.txt"),dge_data_test)
+            random_of_input_file((test_id + "_FHT3794_921_QDx1_24h_Vehicle_921_QDx1_24h_DGE_r30500x10.txt"),dge_data_test)
+            random_of_input_file((test_id + "_FHT3794_921_QDx6_24h_Vehicle_921_QDx6_24h_DGE_r30500x10.txt"),dge_data_test)
+            random_of_input_file((notthistest_id + "_FHT3794_921_QDx6_24h_Vehicle_921_QDx6_24h_DGE_r30500x10.txt"),dge_data_test)
             
             #running the method
             dge_file_list = ph.find_DGE_files(source_dir, test_id)
@@ -114,33 +108,116 @@ class TestGeneralPythonScriptTemplate(unittest.TestCase):
         #that the methods works the are ways that this test could pass but the code isn't working correctly 
         with tempfile.TemporaryDirectory(prefix=temp_wkdir_prefix) as wkdir:
             logger.debug("test_read_DGE_files:  {}".format(wkdir))
-            #creating file and a list to pass into read DGE_files
-            onetwo = create_file_for_testing("onetwo", wkdir)
-            dge_file_list = [onetwo]
+            #creating testod and paths of the directories
+            test_id ="H202SC20040591"
+            source_dir = os.path.join(wkdir,"source_test")
+            dge_data_test = os.path.join(source_dir, "dge_data")
+
+            #creating directories
+            os.mkdir(source_dir)
+            os.mkdir(dge_data_test)
+
+            random_of_input_file((test_id + "_FHT3794_921_QDx1_24h_Vehicle_921_QDx1_24h_DGE_r30500x10.txt"),dge_data_test)
+            random_of_input_file((test_id + "_FHT3794_921_QDx6_24h_Vehicle_921_QDx6_24h_DGE_r30500x10.txt"),dge_data_test)
+
+            dge_file_list = ph.find_DGE_files(source_dir, test_id)
 
             dge_df_list = ph.read_DGE_files(dge_file_list)
             
             for dge_df, dge_file in dge_df_list:
-                    #breaking the test to try something out
-                    pass 
+                    #test the test
+                    self.assertTrue(len(dge_df_list)==2)
+                    
 
-    def prepare_GCToo_objects(self):
+    def test_prepare_GCToo_objects(self):
         #test can be improved, right now it only test when there is one thing in dge_file_list, which while it shows
         #that the methods works the are ways that this test could pass but the code isn't working correctly 
         with tempfile.TemporaryDirectory(prefix=temp_wkdir_prefix) as wkdir:
             logger.debug("test_read_DGE_files:  {}".format(wkdir))
             #creating file and a list to pass into read DGE_files
-            onetwo = create_file_for_testing("onetwo", wkdir)
-            dge_file_list = [onetwo]
+            test_id ="H202SC20040591"
+            source_dir = os.path.join(wkdir,"source_test")
+            dge_data_test = os.path.join(source_dir, "dge_data")
+
+            #creating directories
+            os.mkdir(source_dir)
+            os.mkdir(dge_data_test)
+
+            random_of_input_file((test_id + "_FHT3794_921_QDx1_24h_Vehicle_921_QDx1_24h_DGE_r30500x10.txt"),dge_data_test)
+            random_of_input_file((test_id + "_FHT3794_921_QDx6_24h_Vehicle_921_QDx6_24h_DGE_r30500x10.txt"),dge_data_test)
+
+            dge_file_list = ph.find_DGE_files(source_dir, test_id)
+
+            dge_df_list = ph.read_DGE_files(dge_file_list)
             dge_stats_for_heatmaps = [["logFC", "t"]]
             dge_df_list = ph.read_DGE_files(dge_file_list)
+
             heatmap_gct_list = ph.prepare_GCToo_objects(dge_stats_for_heatmaps, dge_df_list)
 
-    def test_random(self):
-            files = random_of_input_file("test", "test")
-            with open(files) as f:
-                for line in f:
-                    logger.debug(line)
+    def write_GCToo_objects_to_files(self):
+        with tempfile.TemporaryDirectory(prefix=temp_wkdir_prefix) as wkdir:
+            logger.debug("test_write_GCToo_objects_to_files:  {}".format(wkdir))
+            
+            #creating file and a list to pass into read DGE_files
+            test_id ="H202SC20040591"
+            source_dir = os.path.join(wkdir,"source_test")
+            dge_data_test = os.path.join(source_dir, "dge_data")
+
+            #creating directories
+            os.mkdir(source_dir)
+            os.mkdir(dge_data_test)
+
+            random_of_input_file((test_id + "_FHT3794_921_QDx1_24h_Vehicle_921_QDx1_24h_DGE_r30500x10.txt"),dge_data_test)
+            random_of_input_file((test_id + "_FHT3794_921_QDx6_24h_Vehicle_921_QDx6_24h_DGE_r30500x10.txt"),dge_data_test)
+
+            dge_file_list = ph.find_DGE_files(source_dir, test_id)
+
+            dge_df_list = ph.read_DGE_files(dge_file_list)
+            dge_stats_for_heatmaps = [["logFC", "t"]]
+            dge_df_list = ph.read_DGE_files(dge_file_list)
+
+            heatmap_gct_list = ph.prepare_GCToo_objects(dge_stats_for_heatmaps, dge_df_list)
+            
+            output_template = test_id + "_heatmap_{dge_stat}_r{rows}x{cols}.gct"
+            heatmap_dir = source_dir
+
+            ph.write_GCToo_objects_to_files(heatmap_gct_list, output_template, heatmap_dir)
+
+            #run a test to see if it worked 
+
+
+    def prepare_links(self):
+        
+        with tempfile.TemporaryDirectory(prefix=temp_wkdir_prefix) as wkdir:
+            logger.debug("test_prepare_links:  {}".format(wkdir))
+            #creating file and a list to pass into read DGE_files
+            test_id ="H202SC20040591"
+            source_dir = os.path.join(wkdir,"source_test")
+            dge_data_test = os.path.join(source_dir, "dge_data")
+
+            #creating directories
+            os.mkdir(source_dir)
+            os.mkdir(dge_data_test)
+
+            random_of_input_file((test_id + "_FHT3794_921_QDx1_24h_Vehicle_921_QDx1_24h_DGE_r30500x10.txt"),dge_data_test)
+            random_of_input_file((test_id + "_FHT3794_921_QDx6_24h_Vehicle_921_QDx6_24h_DGE_r30500x10.txt"),dge_data_test)
+
+            dge_file_list = ph.find_DGE_files(source_dir, test_id)
+
+            dge_df_list = ph.read_DGE_files(dge_file_list)
+            dge_stats_for_heatmaps = [["logFC", "t"]]
+            dge_df_list = ph.read_DGE_files(dge_file_list)
+
+            heatmap_gct_list = ph.prepare_GCToo_objects(dge_stats_for_heatmaps, dge_df_list)
+            
+            base_data_path = ""
+
+            url_template = "http://fht.samba.data/fht_morpheus.html?gctData={data_path}"
+
+            ph.prepare_links(heatmap_gct_list, url_template, base_data_path)
+
+            #run a test to see that it worked
+
 
             
             
